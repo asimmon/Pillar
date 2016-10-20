89229d9ca3422d0c7b9a6e2f5aaea45728869cdb
Anthony Simmon <simmon.anthony@gmail.com>
2016-10-20 15:34:42 +0200

    Added abstraction of the Xamarin Form's Messenging

---
+++ src/Askaiser.Mobile.Pillar/Interfaces/IMessenger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Askaiser.Mobile.Pillar.Interfaces
+{
+    public interface IMessenger
+    {
+        void Send<TSender, TArgs>(TSender sender, string message, TArgs args)
+            where TSender : class;
+
+        void Send<TSender>(TSender sender, string message)
+            where TSender : class;
+
+        void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = null)
+            where TSender : class;
+
+        void Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source = null)
+            where TSender : class;
+
+        void Unsubscribe<TSender, TArgs>(object subscriber, string message)
+            where TSender : class;
+
+        void Unsubscribe<TSender>(object subscriber, string message)
+            where TSender : class;
+    }
+
+    public sealed class Messenger : IMessenger
+    {
+        public void Send<TSender, TArgs>(TSender sender, string message, TArgs args)
+            where TSender : class
+        {
+            MessagingCenter.Send(sender, message, args);
+        }
+
+        public void Send<TSender>(TSender sender, string message)
+            where TSender : class
+        {
+            MessagingCenter.Send(sender, message);
+        }
+
+        public void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = default(TSender))
+            where TSender : class
+        {
+            MessagingCenter.Subscribe(subscriber, message, callback, source);
+        }
+
+        public void Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source = default(TSender))
+            where TSender : class
+        {
+            MessagingCenter.Subscribe(subscriber, message, callback, source);
+        }
+
+        public void Unsubscribe<TSender, TArgs>(object subscriber, string message)
+            where TSender : class
+        {
+            MessagingCenter.Unsubscribe<TSender, TArgs>(subscriber, message);
+        }
+
+        public void Unsubscribe<TSender>(object subscriber, string message)
+            where TSender : class
+        {
+            MessagingCenter.Unsubscribe<TSender>(subscriber, message);
+        }
+    }
+}
