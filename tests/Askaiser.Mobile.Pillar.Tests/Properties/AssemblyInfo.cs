1c5f9c52b6d39b211d174039d36f3896b33a4d85
Anthony Simmon <simmon.anthony@gmail.com>
2016-11-17 17:45:51 +0100

    Pillar tests project is now a PCL

--- tests/Askaiser.Mobile.Pillar.Tests/Properties/AssemblyInfo.cs
+++ tests/Askaiser.Mobile.Pillar.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
@@ -13,14 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
-// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
-// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
-[assembly: ComVisible(false)]
-
-// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("c5d5d6e6-113d-4880-a1f4-2e5816a3c37c")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
