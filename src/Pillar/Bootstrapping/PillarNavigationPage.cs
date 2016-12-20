4e767252820b1e298d35c6bbb33ba168303f7297
Anthony Simmon <simmon.anthony@gmail.com>
2016-12-20 11:47:49 +0100

    Trying to intercept back button pressed to call pillar's navigator which triggers navigation events on viewmodels

---
+++ src/Pillar/Bootstrapping/PillarNavigationPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Pillar.Interfaces;
+using Pillar.Services;
+using Xamarin.Forms;
+
+namespace Pillar.Bootstrapping
+{
+    /// <summary>
+    /// Custom navigation page that call Pillar's navigator to pop back when
+    /// the back button is pressed. Navigation events will be triggered.
+    /// </summary>
+    /// <seealso cref="INavigationAware"/>
+    internal sealed class PillarNavigationPage : NavigationPage
+    {
+        private readonly INavigator _navigator;
+
+        public PillarNavigationPage(Page root, INavigator navigator)
+            : base(root)
+        {
+            if (navigator == null)
+                throw new ArgumentNullException(nameof(navigator));
+
+            _navigator = navigator;
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            // remove modal page from stack if exists
+            if (CurrentPage.SendBackButtonPressed())
+                return true;
+
+            // TODO ??? check if necessary to keep this base behavior
+            if (Navigation.NavigationStack.Count <= 1)
+                return base.OnBackButtonPressed();
+
+            _navigator.PopAsync().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                    throw task.Exception;
+            });
+
+            return true;
+        }
+    }
+}
