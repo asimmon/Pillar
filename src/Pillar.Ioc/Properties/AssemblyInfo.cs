9fabb54867e8df41a2d4a70ba048ba172d1efcba
Anthony Simmon <simmon.anthony@gmail.com>
2017-03-11 05:01:03 +0100

    Updated assembly info copyright year and min xamarin version required

--- src/Pillar.Ioc/Properties/AssemblyInfo.cs
+++ src/Pillar.Ioc/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Reflection;
 [assembly: AssemblyTitle("Pillar.Ioc")]
 [assembly: AssemblyProduct("Pillar.Ioc")]
 [assembly: AssemblyCompany("Anthony Simmon @ https://anthonysimmon.com")]
-[assembly: AssemblyDescription("A lightweight PCL framework that helps you develop MVVM Xamarin.Forms 1.x / 2.x applications easily.")]
-[assembly: AssemblyCopyright("Copyright © Anthony Simmon 2015-2016")]
+[assembly: AssemblyDescription("A lightweight PCL framework that helps you develop MVVM Xamarin.Forms 2.x applications easily.")]
+[assembly: AssemblyCopyright("Copyright © Anthony Simmon 2015-2017")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: AssemblyVersion("0.0.4")]
