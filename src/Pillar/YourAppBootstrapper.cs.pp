7a7e468ed2a1515341bdd2ad5a54edb5e93a2bea
Anthony Simmon <simmon.anthony@gmail.com>
2017-07-11 11:18:50 -0400

    Include YourAppBootstrapper.cs.pp in nuget package

---
+++ src/Pillar/YourAppBootstrapper.cs.pp
@@ -0,0 +1,42 @@
+using Xamarin.Forms;
+using Pillar;
+
+namespace $rootnamespace$
+{
+    public class YourAppBootstrapper : PillarBootstrapper
+    {
+        // Instantiate this class in your Application class, after InitializeComponent()
+        // new YourAppBootstrapper(this).Run();
+        public YourAppBootstrapper(Application app)
+            : base(app)
+        { }
+
+        protected override void RegisterDependencies(IContainerAdapter container)
+        {
+            // TODO Register your dependencies in the built-in IoC container
+
+            // "New instance" per resolve and "single instance" registration examples
+            // container.RegisterType<Bar, IBar>(); 
+            // container.RegisterSingleton<Foo, IFoo>();
+
+            // Registration without interfaces
+            // container.RegisterType<HomeViewModel>(); 
+            // container.RegisterType<HomePage>();
+        }
+
+        protected override void BindViewModelsToViews(IViewFactory viewFactory)
+        {
+            // TODO Bind your view models to pages
+
+            // viewFactory.Register<HomeViewModel, HomePage>();
+        }
+
+        protected override Page GetFirstPage(IViewFactory viewFactory)
+        {
+            // TODO Return a Xamarin Forms Page resolved by the view factory
+
+            // return viewFactory.Resolve<HomeViewModel>();
+            return null;
+        }
+    }
+}
